a455f052d655f7d5a21ca67c9f49e5cfec88baf9
Gautam Mankatti <gautammankatti@gmail.com>
2023-01-13 20:01:24 -0500

    updated fauna client config

--- netlify/functions/util/connections.js
+++ netlify/functions/util/connections.js
@@ -2,6 +2,9 @@ const faunadb = require('faunadb');
 const q = faunadb.query
 
 const clientQuery = new faunadb.Client({
+  domain: 'db.us.fauna.com',
+  scheme: 'https',
+  port: 443,
   secret: process.env.FAUNADB_SERVER_SECRET,
 });
 
