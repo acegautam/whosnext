dca63e81d0b4336caeb000d049bb672078b3c5f1
Gautam Mankatti <gautam.mankatti@lowes.com>
2023-01-16 22:13:35 -0500

    fixed logic issues

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -11,37 +11,32 @@ import {
 import { FolksReponseType } from '../../types/common';
 
 const Board: React.FC = () => {
+  const [allFolks, setAllFolks] = useState([]);
   const [folks, setFolks] = useState([]);
   const [chosenOne, setChosenOne] = useState('');
   const [selected, setSelected] = useState('');
 
-  const fetchFolks = async () => {
-    // get all participants
-    const response = await fetchParticipants();
-    if (!response?.data) return;
-    const allData = response.data.map(
-      (item: FolksReponseType) => item.data.name
-    );
-
-    // get all presented
-    const presented = await fetchPresented();
-    const presentedData =
-      presented?.data?.map((item: FolksReponseType) => item.data.name) || [];
-
-    if (presentedData.length === allData.length) {
-      console.log('======= Resetting now... =========');
-      await resetPresented();
-      await fetchFolks();
-      return;
-    }
-
-    // filter off presented guys
-    const folksData =
-      allData.filter((name: string) => !presentedData.includes(name)) || [];
-    setFolks(folksData);
-  };
-
   useEffect(() => {
+    const fetchFolks = async () => {
+      // get all participants
+      const response = await fetchParticipants();
+      if (!response?.data) return;
+      const allData = response.data.map(
+        (item: FolksReponseType) => item.data.name
+      );
+      setAllFolks(allData);
+
+      // get all presented
+      const presented = await fetchPresented();
+      const presentedData =
+        presented?.data?.map((item: FolksReponseType) => item.data.name) || [];
+
+      // filter off presented guys
+      const folksData =
+        allData.filter((name: string) => !presentedData.includes(name)) || [];
+      setFolks(folksData);
+    };
+
     fetchFolks();
   }, []);
 
@@ -61,10 +56,16 @@ const Board: React.FC = () => {
   };
 
   const accept = async () => {
+    if (folks.length === 1) {
+      setFolks(allFolks);
+      await resetPresented();
+      return;
+    }
+    const filteredFolks = folks.filter((f) => f !== chosenOne);
+    setFolks(filteredFolks);
     await updateChosenOne(chosenOne);
     setSelected('');
     setChosenOne('');
-    await fetchFolks();
   };
   return (
     <StyledBoard>
