ed8b8cf942e4317d1b92a87f092164930be8f291
Gautam Mankatti <gautam.mankatti@lowes.com>
2023-01-22 15:44:46 -0500

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import "@fontsource/amatic-sc";
+import '@fontsource/amatic-sc';
 import './App.css';
 import Board from './components/Board/Board';
 
@@ -15,9 +15,22 @@ function App() {
         <Board />
       </main>
       <footer>
-      <a href="https://www.flaticon.com/free-icons/bot" target='_blank' title="bot icons" rel="noreferrer">Bot icons by Smashicons - Flaticon</a>
+        <small>
+          Made with <div className='heart'>&hearts;</div> by{' '}
+          <a href='mailto:gautam.mankatti@lowes.com'>
+            <span className='dude'>Gautam Mankatti</span>
+          </a>
+        </small>
+        <a
+          className='icons-credit'
+          href='https://www.flaticon.com/free-icons/bot'
+          target='_blank'
+          title='bot icons'
+          rel='noreferrer'
+        >
+          Bot icons by Smashicons - Flaticon
+        </a>
       </footer>
-      
     </div>
   );
 }
