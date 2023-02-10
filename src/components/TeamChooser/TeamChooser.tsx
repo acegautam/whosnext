012801eed32dffa3ed656014b5afa34ebe5fde4f
Gautam Mankatti <gautam.mankatti@lowes.com>
2023-02-09 19:38:55 -0500

    fixed lint errors

--- src/components/TeamChooser/TeamChooser.tsx
+++ src/components/TeamChooser/TeamChooser.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Card, CardContent } from '@mui/material';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
